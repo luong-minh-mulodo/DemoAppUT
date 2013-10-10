894814a5078c5759266397d3be0e32e02b5c7044
Luong Cam Minh <luongcamminh@Jin-Ming-MacBook-Pro.local>
2013-10-10 15:34:28 +0700

    compare code

--- DemoAppUT/src/jp/mulodo/demoapput/object/Login.java
+++ DemoAppUT/src/jp/mulodo/demoapput/object/Login.java
@@ -20,5 +20,39 @@ public class Login {
 		Password = password;
 	}
 	
+	public String getUserName() {
+		return UserName;
+	}
+	
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	
+	public String getPassword() {
+		return Password;
+	}
+	
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+	
+	public String getUserName() {
+		return UserName;
+	}
+	
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	
+	public String getPassword() {
+		return Password;
+	}
+	
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+	
 
 }
