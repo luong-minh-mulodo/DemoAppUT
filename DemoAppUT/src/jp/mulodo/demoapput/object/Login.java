8ee14c227e247368ebab9df14d96c70e20cdd033
nguyen-thuong-mulodo <nguyen.thuong@mulodo.com>
2013-10-10 15:39:24 +0700

    kop commit dep trai

--- DemoAppUT/src/jp/mulodo/demoapput/object/Login.java
+++ DemoAppUT/src/jp/mulodo/demoapput/object/Login.java
@@ -19,40 +19,5 @@ public class Login {
 	public void setPassword(String password) {
 		Password = password;
 	}
-	
-	public String getUserName() {
-		return UserName;
-	}
-	
-	public void setUserName(String userName) {
-		UserName = userName;
-	}
-	
-	public String getPassword() {
-		return Password;
-	}
-	
-	public void setPassword(String password) {
-		Password = password;
-	}
-	
-	
-	public String getUserName() {
-		return UserName;
-	}
-	
-	public void setUserName(String userName) {
-		UserName = userName;
-	}
-	
-	public String getPassword() {
-		return Password;
-	}
-	
-	public void setPassword(String password) {
-		Password = password;
-	}
-	
-	
 
 }
