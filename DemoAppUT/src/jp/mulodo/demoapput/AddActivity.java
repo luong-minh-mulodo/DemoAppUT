2a7352398eafc19fff67e42e77905f0a83a4586f
nguyenvantu <nguyenvantu@192.168.2.93>
2013-10-11 16:42:26 +0700

    add event modify

--- DemoAppUT/src/jp/mulodo/demoapput/AddActivity.java
+++ DemoAppUT/src/jp/mulodo/demoapput/AddActivity.java
@@ -8,20 +8,20 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class AddActivity extends FragmentActivity implements OnClickListener{
-	private Button mBt_save;
+	private Button mBt_add;
 	private EditText mEdt_name,mEdt_address,mEdt_email,mEdt_detail;
 	@Override
 	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_add);
-		mBt_save = (Button)findViewById(R.id.add_btn_add);
+		mBt_add = (Button)findViewById(R.id.add_btn_add);
 		mEdt_name = (EditText)findViewById(R.id.add_edt_name);
 		mEdt_address = (EditText)findViewById(R.id.add_edt_address);
 		mEdt_email = (EditText)findViewById(R.id.add_edt_email);
 		mEdt_detail = (EditText)findViewById(R.id.add_edt_detail);
 		
-		mBt_save.setOnClickListener(this);
+		mBt_add.setOnClickListener(this);
 	}
 	@Override
 	public void onClick(View v) {
