ea609cb6c8e5f29d10fb8bbe5dd6d834a19606f6
nguyenvantu <nguyenvantu@192.168.2.93>
2013-10-11 15:53:27 +0700

    add event mainactivity

--- DemoAppUT/src/jp/mulodo/demoapput/MainActivity.java
+++ DemoAppUT/src/jp/mulodo/demoapput/MainActivity.java
@@ -1,16 +1,26 @@
 package jp.mulodo.demoapput;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements OnClickListener {
+	private Button mBt_add, mBt_list;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//
 		setContentView(R.layout.activity_main);
+		mBt_add = (Button) findViewById(R.id.mainactivity_btn_add);
+		mBt_list = (Button) findViewById(R.id.mainactivity_btn_list);
+
+		mBt_add.setOnClickListener(this);
+		mBt_list.setOnClickListener(this);
 		// Test Commit
 	}
 
@@ -20,10 +30,27 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void testApp()
-	{
+
+	public void testApp() {
 		// test app
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.mainactivity_btn_add:
+			Intent i = new Intent(getApplicationContext(),AddActivity.class);
+			startActivity(i);
+			break;
+		case R.id.mainactivity_btn_list:
+			Intent i1 = new Intent(getApplicationContext(),ListCustomersActivity.class);
+			startActivity(i1);
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
