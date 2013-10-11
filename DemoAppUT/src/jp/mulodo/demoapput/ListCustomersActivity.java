6958407a13ef06626365d0b733fc65fe4a5d14e9
nguyenvantu <nguyenvantu@192.168.2.93>
2013-10-11 16:09:16 +0700

    add event listcustom

--- DemoAppUT/src/jp/mulodo/demoapput/ListCustomersActivity.java
+++ DemoAppUT/src/jp/mulodo/demoapput/ListCustomersActivity.java
@@ -1,7 +1,16 @@
 package jp.mulodo.demoapput;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.ListView;
 
 public class ListCustomersActivity extends FragmentActivity{
-
+	private ListView mLv_user;
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_listcustomer);
+		mLv_user = (ListView)findViewById(R.id.listcustom_lv_user);
+	}
 }
