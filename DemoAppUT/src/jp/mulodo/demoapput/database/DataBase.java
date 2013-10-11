9e63ce07779e9f75000903b0c987297adf8d77da
nguyen-thuong-mulodo <nguyen.thuong@mulodo.com>
2013-10-11 15:18:04 +0700

    change name db

--- DemoAppUT/src/jp/mulodo/demoapput/database/DataBase.java
+++ DemoAppUT/src/jp/mulodo/demoapput/database/DataBase.java
@@ -6,23 +6,23 @@ import jp.mulodo.demoapput.object.CustomerInfo;
 import jp.mulodo.demoapput.object.User;
 
 public class DataBase {
-	private ArrayList<CustomerInfo> listCutomers;
-	private ArrayList<User> listUsers;
+	private ArrayList<CustomerInfo> lCutomers;
+	private ArrayList<User> lUsers;
 
 	public DataBase() {
-		listCutomers = new ArrayList<CustomerInfo>();
-		listUsers = new ArrayList<User>();
+		lCutomers = new ArrayList<CustomerInfo>();
+		lUsers = new ArrayList<User>();
 		for (int i = 0; i < 20; ++i) {
 			CustomerInfo customer = new CustomerInfo();
 			customer.setName("Nguyen Van " + i);
 			customer.setAddress((i * 2 + 1) + " vo van tan");
-			listCutomers.add(customer);
+			lCutomers.add(customer);
 			User user = new User();
 			user.setLogged(false);
 			user.setId(i+"");
 			user.setUserName("mr_"+i);
 			user.setPassword("123"+i);
-			listUsers.add(user);
+			lUsers.add(user);
 		}
 
 	}
