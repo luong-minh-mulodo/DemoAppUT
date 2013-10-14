deb6f3722d213de98a00e61f718174fb34c068d0
nguyenvantu <nguyenvantu@192.168.2.80>
2013-10-14 10:43:45 +0700

    finish deleteActivity, ListCustomActivity

---
+++ DemoAppUT/src/jp/mulodo/demoapput/adapter/Adapter_ListCustom.java
@@ -0,0 +1,119 @@
+package jp.mulodo.demoapput.adapter;
+
+import java.util.List;
+
+import jp.mulodo.demoapput.R;
+import jp.mulodo.demoapput.object.CustomerInfo;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Adapter_ListCustom  extends BaseAdapter<CustomerInfo>{
+	
+	private List<CustomerInfo> mArr;
+	private LayoutInflater inflater;
+	private OnClickListListener listener;
+	private int position;
+	private int layout;
+	private Context mContext;
+	public Adapter_ListCustom(Context context, int textViewResourceId,
+			List<CustomerInfo> objects,OnClickListListener listener) {
+		super(context, textViewResourceId, objects);
+		// TODO Auto-generated constructor stub
+		this.listener = listener;
+		this.layout = textViewResourceId;
+		this.mArr = objects;
+		this.inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		this.mContext = context;
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.ArrayAdapter#getCount()
+	 */
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return super.getCount();
+	}
+
+	/**
+	 * add new list for adapter
+	 * 
+	 * @method public
+	 * @return void
+	 */
+	public void addItems(List<CustomerInfo> list) {
+		mArr.addAll(list);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.ArrayAdapter#getView(int, android.view.View,
+	 * android.view.ViewGroup)
+	 */
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+
+		return getViewPuzzle(position, convertView);
+
+	}
+
+	class ViewHolder {
+		Button btn_delete;
+		TextView tv_mName;
+	}
+
+	public static interface OnClickListListener {
+		public abstract void onClick(CustomerInfo data);
+	}
+
+	private View getViewPuzzle(final int position, View convertView) {
+		View view = convertView;
+		ViewHolder mHolder = null;
+		
+		if (view == null) {
+			// ??????????????????????????????????????????????????????????????????r???????????????????????????[??????????????????null????????????????????????V?????????????????????????????????????????????r???????????????????????????[??????????????????????????????
+			view = inflater.inflate(layout, null);
+			mHolder = new ViewHolder();
+			
+			mHolder.btn_delete = (Button) view.findViewById(R.id.listitem_btn_delete);
+			mHolder.tv_mName = (TextView) view
+					.findViewById(R.id.listitem_tv_name);
+			view.setTag(mHolder);
+		} else {
+			mHolder = (ViewHolder) view.getTag();
+		}
+
+		if (mArr.size() > 0) {
+			
+			final CustomerInfo top = mArr.get(position);
+			mHolder.tv_mName.setText(top.getName());
+			if (listener != null) {
+				mHolder.btn_delete.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(final View view) {
+						setPosition(position);
+						listener.onClick(top);
+					}
+				});
+			}
+		}
+
+		return view;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+}
