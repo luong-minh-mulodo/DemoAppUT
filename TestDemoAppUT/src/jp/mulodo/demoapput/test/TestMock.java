93ba4bc2ac0ff60ae4431b226898f6a2162240cb
Luong Cam Minh <luongcamminh@Jin-Ming-MacBook-Pro.local>
2013-10-14 16:50:08 +0700

    config travis ci

--- TestDemoAppUT/src/jp/mulodo/demoapput/test/TestMock.java
+++ TestDemoAppUT/src/jp/mulodo/demoapput/test/TestMock.java
@@ -2,43 +2,16 @@ package jp.mulodo.demoapput.test;
 
 import java.util.List;
 
-import org.easymock.EasyMock;
-import org.mockito.Mockito;
-
-import jp.mulodo.demoapput.object.CustomerInfo;
 import jp.mulodo.demoapput.object.User;
 import junit.framework.TestCase;
 
 public class TestMock extends TestCase {
 
-	private List customerInfoList;
-	private List<User> userList;
-	
 	@Override
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		super.setUp();
-//		customerInfoList = EasyMock.createNiceMock(List.class);
-//		EasyMock.expect(customerInfoList .get(0)).andStubReturn("one");                             
-//		EasyMock.expect(customerInfoList .get(1)).andStubReturn("two");
-//		customerInfoList.clear();
-//		  
-//		EasyMock.replay(customerInfoList);
-//
-//		EasyMock.verify(customerInfoList); 
 
-
-//		userList = EasyMock.createMock(List.class);
-		
-//		for (int i = 0; i < 20; ++i) {
-//			CustomerInfo customer = new CustomerInfo();
-//			customer.setName("Nguyen Van " + i);
-//			customer.setAddress((i * 2 + 1) + " vo van tan");
-//			customer.setEmail("abc"+i+"@a.com");
-//			customer.setDetail("friend");
-//			customerInfoList.add(customer);
-//		}
-		
 	}
 	
 	public void testData()
